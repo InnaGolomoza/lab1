f759727524dbc9bd9bf5c5f31fa6d5b1d3947c7d
InnaGolomoza <154058061+InnaGolomoza@users.noreply.github.com>
2023-12-21 11:39:47 +0200

    Update lab1.4.py

--- lab1.4.py
+++ lab1.4.py
@@ -1,15 +1,21 @@
 """Користувач вводить рядок, у якому чергуються цифри та інші символи.
 На початку і у кінці рядка цифри відсутні. Напишіть програму, яка друкує усі
 символи введеного рядка у тому ж порядку, але без цифр.
-#Голомоза Інна Андріївна"""
+Голомоза Інна Андріївна"""
 
 #Введення данних
 s = input("Введіть рядок: ") 
 
-result = ''
-for i in s:
-    if i not in ('0','1','2','3','4','5','6','7','8','9'):
-        result += i
+result = s.replace("0","")
+result = result.replace("1","")
+result = result.replace("2","")
+result = result.replace("3","")
+result = result.replace("4","")
+result = result.replace("5","")
+result = result.replace("6","")
+result = result.replace("7","")
+result = result.replace("8","")
+result = result.replace("9","")
    
 #Виведення данних
 print("Результат: ", result)
